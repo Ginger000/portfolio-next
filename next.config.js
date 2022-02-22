ad2c77edc9f7a50b0f620c133b453692eb410095
Jiang, Lin <linj2@illinois.edu>
2022-02-22 12:01:51 -0600

    test analytics

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   reactStrictMode: true,
+  env:{
+    GOOGLE_ANALYTICS_ID:'G-LRC4934Z3B'
+  }
 }
--- pages/_app.js
+++ pages/_app.js
@@ -17,17 +17,17 @@ import Script from 'next/script';
 function MyApp({ Component, pageProps }) {
     return (
         <>
-            {/* <Script
-                strategy="lazyOnload"
-                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+            <Script
+                strategy="afterInteractive"
+                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`}
             />
 
-            <Script id="my-script" strategy="lazyOnload">
+            <Script id="google-analytics-script" strategy="afterInteractive">
                 {`window.dataLayer = window.dataLayer || [];
                   function gtag(){dataLayer.push(arguments);}
                   gtag('js', new Date());
-                  gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});`}
-            </Script> */}
+                  gtag('config', '${process.env.GOOGLE_ANALYTICS_ID}');`}
+            </Script>
             <Navbar />
             <Component {...pageProps} />
             <Footer />
