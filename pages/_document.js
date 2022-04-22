9c036406cc681606fcae99a3efe0b0d7149abd87
Jiang, Lin <linj2@illinois.edu>
2022-04-22 15:04:22 -0500

    add image to meta data

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,11 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <meta
+          name='image'
+          property='og:image'
+          content='https://res.cloudinary.com/dsehzh5ha/image/upload/v1645053667/large_Screenshot_2022_02_07_152941_5268de56e5.png'
+        />
       {/* <title>Ginger's Portfolio</title>
       <link rel="shortcut icon" href="/paper_cyan_favicon.ico" /> */}
       </Head>
