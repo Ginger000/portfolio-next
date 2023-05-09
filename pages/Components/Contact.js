781cce7a6fa7c202cbf08a9c96ee40b5975c77eb
gingerBBB <ljiang@beaconfireinc.com>
2023-05-09 16:00:05 -0400

    replace resume and email

--- pages/Components/Contact.js
+++ pages/Components/Contact.js
@@ -1,12 +1,12 @@
-import React, { useEffect } from 'react';
-import Button from './Button';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebookSquare } from '@fortawesome/free-brands-svg-icons';
-import { faInstagram } from '@fortawesome/free-brands-svg-icons';
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
-import { Icon } from '@iconify/react';
-import { motion, useAnimation } from 'framer-motion';
-import { useInView } from 'react-intersection-observer';
+import React, { useEffect } from "react";
+import Button from "./Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebookSquare } from "@fortawesome/free-brands-svg-icons";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { Icon } from "@iconify/react";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
 const Contact = () => {
     const { ref, inView } = useInView({
@@ -33,7 +33,7 @@ const Contact = () => {
         // console.log("use effect hook, inView = ", inView)
     }, [inView]);
     const sendEmail = () => {
-        window.location = 'mailto:linj2@illinois.edu';
+        window.location = "mailto:ginger23333@gmail.com";
     };
     return (
         <motion.div
@@ -46,37 +46,57 @@ const Contact = () => {
             </h1>
             <div>
                 <Button handleOnClick={sendEmail}>
-                    {' '}
-                    Email: linj2@illinois.edu{' '}
+                    {" "}
+                    Email: ginger23333@gmail.com{" "}
                 </Button>
             </div>
             <div>Phone: 217-904-0965</div>
             <div className="mt-6 flex space-x-3 text-2xl justify-center items-center text-gray-300">
-                <a href="https://www.facebook.com/profile.php?id=100009390374545" target="_blank" rel="noreferrer noopener">
+                <a
+                    href="https://www.facebook.com/profile.php?id=100009390374545"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                >
                     <FontAwesomeIcon
                         className="hover:text-hyper-cyan text-gray-200 duration-500 cursor-pointer"
                         icon={faFacebookSquare}
                     ></FontAwesomeIcon>
                 </a>
-                <a href="https://www.instagram.com/ginger_linjiang/?hl=en" target="_blank" rel="noreferrer noopener">
+                <a
+                    href="https://www.instagram.com/ginger_linjiang/?hl=en"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                >
                     <FontAwesomeIcon
                         className="hover:text-hyper-cyan text-gray-200 duration-500 cursor-pointer"
                         icon={faInstagram}
                     ></FontAwesomeIcon>
                 </a>
-                <a href="https://github.com/Ginger000" target="_blank" rel="noreferrer noopener">
+                <a
+                    href="https://github.com/Ginger000"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                >
                     <FontAwesomeIcon
                         className="hover:text-hyper-cyan text-gray-200 duration-500 cursor-pointer"
                         icon={faGithub}
                     ></FontAwesomeIcon>
                 </a>
-                <a href="https://leetcode.com/ginger23333/" target="_blank" rel="noreferrer noopener">
+                <a
+                    href="https://leetcode.com/ginger23333/"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                >
                     <Icon
                         className="hover:text-hyper-cyan text-gray-200 duration-500 cursor-pointer"
                         icon="simple-icons:leetcode"
                     />
                 </a>
-                <a href="https://issuu.com/ginger_linjiang/docs/lin_jiang_s_portfolio" target="_blank" rel="noreferrer noopener">
+                <a
+                    href="https://issuu.com/ginger_linjiang/docs/lin_jiang_s_portfolio"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                >
                     <Icon
                         className="hover:text-hyper-cyan text-gray-200 duration-500 cursor-pointer"
                         icon="cib:issuu"
